dba5f54c18f4012d1174fd71db13f76d4e80a92b
karthikt04 <karthik04t@gmail.com>
2025-02-11 17:50:55 +0530

    nested for

--- Nomain.java
+++ Nomain.java
@@ -4,16 +4,20 @@ import java.util.Scanner;
 public class Nomain {
     public static void main(String args[]){
         Scanner scan = new Scanner(System.in);
-        System.out.print("enter your item: ");
-        String Item = scan.nextLine();
-        System.out.print("enter price per item: $");
-        double Price = scan.nextDouble();
-        scan.nextLine();
-        System.out.print("enter quantity: ");
-        int Quantity = scan.nextInt();
-        scan.nextLine();
-        System.out.println("you have bought "+Quantity+" "+Item+"/s");
-        System.out.println("your total price: $"+Quantity*Price);
+        System.out.print("enter the symbol: ");
+        String symbol = scan.nextLine();
+        System.out.print("enter the no of rows: ");
+        int row = scan.nextInt();
+
+        System.out.print("enter the no of columns: ");
+        int columns = scan.nextInt();
+
+        for(int i= 0 ; i<row; i++){
+            for(int j=0;j<columns;j++){
+                System.out.print(symbol);
+            }
+            System.out.println();
+        }
         scan.close();
     }
 }
